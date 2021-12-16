9ea988c8fbb33049734d93eb529bce3df3fbca78
Zanco1993 <zanconatofabio@gmail.com>
2021-12-16 16:27:28 +0100

    implementato le funzioni manca il risultato finale

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -10,7 +10,17 @@ function number(min, max){
     return result;
 }
 
+function pariDispari(sommaNumeri){
+    if (sommaNumeri % 2 === 0) {
+        console.log("La somma è PARI");
+    } else {
+        console.log("La somma è DISPARI");
+    }
+}
 
+
+const choosePariDispari = prompt("Inserisci la tua scelta: Pari o Dispari?");
+console.log(`L'utente ha scelto ${choosePariDispari}`);
 
 const numUser = parseInt(prompt("Inserisci un numero da  1 a 5"));
 
@@ -18,8 +28,22 @@ const numUser = parseInt(prompt("Inserisci un numero da  1 a 5"));
 // voglio escludere i negativi, lo 0 ed i numeri maggiori di 5
 if (numUser < 1 || numUser > 5) {
     console.log("Il numero inserito è maggiore di 5. RITENTA!!!")
+} 
+
+console.log(`Il numero inserito dall'utente è ${numUser}`);
+const numMachine = number(1, 5);
+console.log(`Il numero inserito dalla macchina casualmente è ${numMachine}`);
+
+let sumNumber = numUser + numMachine;
+console.log(`La somma dei numeri inseriti è ${sumNumber}`);
+
+if (choosePariDispari.toLowerCase() === "pari" && sumNumber % 2 === 0) {
+    console.log("L'utente ha vinto!!!")
+} else if (choosePariDispari.toLowerCase() === "dispari" && sumNumber % 2 === 0) {
+    console.log("La macchina ha vinto!!!")
+    
 } else {
-    console.log(`Il numero inserito dall'utente è ${numUser}`);
-    const numMachine = number(1, 5);
-    console.log(`Il numero inserito dalla macchina casualmente è ${numMachine}`);
+    console.log("HAI SBAGLIATO AD INSERIRE I DATI!!! RITENTA!!!")
 }
+
+
